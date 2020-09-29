d579149fafe99c307386c5543d2aefac96aeed32
aozbatman <ozbatman@netas.com.tr>
2020-09-29 21:24:16 +0300

    refactoring: Boolean.TRUE used instead of true.

--- src/main/java/com/trendyol/toyrobot/config/CouchbaseIndexConfiguration.java
+++ src/main/java/com/trendyol/toyrobot/config/CouchbaseIndexConfiguration.java
@@ -22,6 +22,6 @@ public class CouchbaseIndexConfiguration {
 		QueryIndexManager indexManager = couchbaseCluster.queryIndexes();
 
 		indexManager.createPrimaryIndex(couchbaseProperties.getBucketName(),
-				CreatePrimaryQueryIndexOptions.createPrimaryQueryIndexOptions().ignoreIfExists(true));
+				CreatePrimaryQueryIndexOptions.createPrimaryQueryIndexOptions().ignoreIfExists(Boolean.TRUE));
 	}
 }
