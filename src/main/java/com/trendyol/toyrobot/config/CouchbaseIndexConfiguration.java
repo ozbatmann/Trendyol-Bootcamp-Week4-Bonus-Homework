ceb43a0054cdedaa006073e03def135cccaf4426
aozbatman <ozbatman@netas.com.tr>
2020-09-29 21:16:46 +0300

    chore: indentation fixed.

--- src/main/java/com/trendyol/toyrobot/config/CouchbaseIndexConfiguration.java
+++ src/main/java/com/trendyol/toyrobot/config/CouchbaseIndexConfiguration.java
@@ -2,31 +2,26 @@ package com.trendyol.toyrobot.config;
 
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.manager.query.CreatePrimaryQueryIndexOptions;
-import com.couchbase.client.java.manager.query.CreateQueryIndexOptions;
 import com.couchbase.client.java.manager.query.QueryIndexManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
-
 @Configuration
 public class CouchbaseIndexConfiguration {
 
-    private final Cluster couchbaseCluster;
-    private final CouchbaseProperties couchbaseProperties;
+	private final Cluster couchbaseCluster;
+	private final CouchbaseProperties couchbaseProperties;
 
-    public CouchbaseIndexConfiguration(Cluster couchbaseCluster, CouchbaseProperties couchbaseProperties) {
-        this.couchbaseCluster = couchbaseCluster;
-        this.couchbaseProperties = couchbaseProperties;
-    }
+	public CouchbaseIndexConfiguration(Cluster couchbaseCluster, CouchbaseProperties couchbaseProperties) {
+		this.couchbaseCluster = couchbaseCluster;
+		this.couchbaseProperties = couchbaseProperties;
+	}
 
-    @Bean
-    public void createIndexes() {
-        QueryIndexManager indexManager = couchbaseCluster.queryIndexes();
+	@Bean
+	public void createIndexes() {
+		QueryIndexManager indexManager = couchbaseCluster.queryIndexes();
 
-        if(indexManager.getAllIndexes(couchbaseProperties.getBucketName()).isEmpty()){
-            indexManager.createPrimaryIndex(couchbaseProperties.getBucketName(),
-                    CreatePrimaryQueryIndexOptions.createPrimaryQueryIndexOptions());
-        }
-    }
+		indexManager.createPrimaryIndex(couchbaseProperties.getBucketName(),
+				CreatePrimaryQueryIndexOptions.createPrimaryQueryIndexOptions().ignoreIfExists(true));
+	}
 }
